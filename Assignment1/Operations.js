8f933aa2d7ab28e3d652d339e4e0b65886be5277
adbre <adbrenes05@gmail.com>
2026-09-02 13:09:24 -0400

    Let replacing var

--- Assignment1/Operations.js
+++ Assignment1/Operations.js
@@ -1,8 +1,8 @@
 
 //Problem 1
-var P1 = 30
-var P2 = 50
-var P3= 40
+let P1 = 30
+let P2 = 50
+let P3= 40
 
 console.log((0.4*P1) + (0.4*P2) + (0.2*P3))
 
@@ -13,8 +13,8 @@ console.log(weightGrade(30,50,40))
 
 
 //Problem 2
-var tempCel = 2
-var tempFar = (tempCel * (9/5) + 32)
+let  tempCel = 2
+let tempFar = (tempCel * (9/5) + 32)
 console.log(tempFar)
 
 function celToFar(cel){
@@ -23,8 +23,8 @@ function celToFar(cel){
 console.log(celToFar(2))
 
 //Problem 3
-var weightPounds = 120
-var weightKilo = weightPounds / 2.205
+let weightPounds = 120
+let weightKilo = weightPounds / 2.205
 console.log(weightKilo)
 
 function poundsToKilo(pounds){
@@ -33,9 +33,9 @@ function poundsToKilo(pounds){
 console.log(poundsToKilo(120))
 
 //Problem 4
-var feet = 3
-var inches = 8
-var meters = (feet * 0.3048) + (inches * 0.0254)
+let feet = 3
+let inches = 8
+let meters = (feet * 0.3048) + (inches * 0.0254)
 console.log(meters)
 
 function imperialToMetric(feet, inches){
@@ -44,9 +44,9 @@ function imperialToMetric(feet, inches){
 console.log(imperialToMetric(3, 8))
 
 //Problem 5
-var weight = 100 //in pounds
-var height = 120 //in inches
-var bmi = weight / (height*height) * 703
+let weight = 100 //in pounds
+let height = 120 //in inches
+let bmi = weight / (height*height) * 703
 console.log(bmi)
 
 function bmiCalc(weight, height){
